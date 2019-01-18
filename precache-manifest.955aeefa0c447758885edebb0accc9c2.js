4e0ceec1aa02a6a5130f2161ea011116ec5bc0a7
devil-geek <devil.prowler@gmail.com>
2019-01-17 19:45:36 -0600

    Updates

---
+++ precache-manifest.955aeefa0c447758885edebb0accc9c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9c2598ef04e5c1f2fa18",
+    "url": "/static/js/main.9c2598ef.chunk.js"
+  },
+  {
+    "revision": "fa035aaceac97e7a3f73",
+    "url": "/static/js/1.fa035aac.chunk.js"
+  },
+  {
+    "revision": "9c2598ef04e5c1f2fa18",
+    "url": "/static/css/main.6b540cbe.chunk.css"
+  },
+  {
+    "revision": "aea160aefe9cb1967b41e5468583acd3",
+    "url": "/index.html"
+  }
+];
